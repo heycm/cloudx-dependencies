fc895593e1899bcca01d38414d9c518cd476cad4
heycm <heycm@qq.com>
2025-08-01 00:17:19 +0800

    first commit

---
+++ ddd4j-core/src/main/java/cn/heycm/d3framework/Main.java
@@ -0,0 +1,14 @@
+package cn.heycm.d3framework;
+
+/**
+ * ${desc}
+ * @author heycm
+ * @version 1.0
+ * @since 2025/7/31 23:46
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
