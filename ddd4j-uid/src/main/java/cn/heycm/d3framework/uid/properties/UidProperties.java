e78610700b6ad550aa199d19f3e9ce6d098e0759
heycm <heycm@qq.com>
2025-08-26 23:12:02 +0800

    组件：uid

---
+++ ddd4j-uid/src/main/java/cn/heycm/d3framework/uid/properties/UidProperties.java
@@ -0,0 +1,65 @@
+package cn.heycm.d3framework.uid.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * UID 配置，支持配置雪花ID各部分位数，可配置共63位，首位符号位为0不用配置
+ * @author heycm
+ * @version 1.0
+ * @since 2025/8/26 22:11
+ */
+@ConfigurationProperties(prefix = "ddd4j.uid.worker-id")
+public class UidProperties {
+
+    /**
+     * 机器ID，要求全局唯一
+     */
+    private int workerId;
+
+    /**
+     * 时间位，最大可表示秒数，默认 30 位，可使用 2^30 秒约为 34 年
+     */
+    private int timeBits = 30;
+
+    /**
+     * 机器位，最大可表示机器数，默认 14 位，可支持 2^14=16384 台机器使用
+     */
+    private int workerBits = 14;
+
+    /**
+     * 序列位，同一秒内单节点可生成的ID数，默认 19 位，可支持 2^19=524288 个ID，按默认配置理论上全机器每秒支持并发量达 16384*524288≈85.9亿
+     */
+    private int seqBits = 19;
+
+    public int getWorkerId() {
+        return workerId;
+    }
+
+    public void setWorkerId(int workerId) {
+        this.workerId = workerId;
+    }
+
+    public int getTimeBits() {
+        return timeBits;
+    }
+
+    public void setTimeBits(int timeBits) {
+        this.timeBits = timeBits;
+    }
+
+    public int getWorkerBits() {
+        return workerBits;
+    }
+
+    public void setWorkerBits(int workerBits) {
+        this.workerBits = workerBits;
+    }
+
+    public int getSeqBits() {
+        return seqBits;
+    }
+
+    public void setSeqBits(int seqBits) {
+        this.seqBits = seqBits;
+    }
+}
---
+++ ddd4j-uid/src/main/java/cn/heycm/d3framework/uid/service/UidService.java
@@ -0,0 +1,17 @@
+package cn.heycm.d3framework.uid.service;
+
+/**
+ * UID服务
+ * @author heycm
+ * @version 1.0
+ * @since 2025/8/26 22:56
+ */
+public interface UidService {
+
+    /**
+     * 生成ID
+     * @return ID
+     */
+    long nextId();
+
+}
