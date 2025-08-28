98542a3e00ec4604b0216c6a8ab1d27e20cc9fe5
heycm <heycm@qq.com>
2025-08-28 22:30:13 +0800

    json更新

---
+++ ddd4j-core/src/main/java/cn/heycm/d3framework/core/contract/exception/JsonException.java
@@ -0,0 +1,14 @@
+package cn.heycm.d3framework.core.contract.exception;
+
+/**
+ * Json操作异常
+ * @author heycm
+ * @version 1.0
+ * @since 2025/8/28 21:52
+ */
+public class JsonException extends RuntimeException {
+
+    public JsonException(Throwable t) {
+        super(t);
+    }
+}
--- ddd4j-core/src/main/java/cn/heycm/d3framework/core/utils/Jackson.java
+++ ddd4j-core/src/main/java/cn/heycm/d3framework/core/utils/Jackson.java
@@ -1,16 +1,23 @@
 package cn.heycm.d3framework.core.utils;
 
+import cn.heycm.d3framework.core.contract.exception.JsonException;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import java.io.IOException;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.fasterxml.jackson.module.afterburner.AfterburnerModule;
 import java.text.SimpleDateFormat;
-import java.util.Collection;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.TimeZone;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -20,29 +27,40 @@ import lombok.extern.slf4j.Slf4j;
  * @since 2025-3-22 20:40
  */
 @Slf4j
-public class Jackson {
+public final class Jackson {
 
     private static final String TIME_PATTERN = "yyyy-MM-dd HH:mm:ss";
+    private static final String TIME_ZONE = "Asia/Shanghai";
 
     private Jackson() {
+        throw new UnsupportedOperationException("Utility class");
     }
 
-    private static final ObjectMapper OBJECT_MAPPER = defaultObjectMapper();
+    private static final ObjectMapper MAPPER = defaultObjectMapper();
 
     public static ObjectMapper defaultObjectMapper() {
-        ObjectMapper objectMapper = new ObjectMapper();
+        ObjectMapper om = new ObjectMapper();
+        om.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES); // 忽略json存在但Java对象中不存在的字段
+        om.disable(SerializationFeature.WRITE_DATE_KEYS_AS_TIMESTAMPS);  // 关闭默认转换timestamps格式
+        om.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);  // 关闭默认转换timestamps格式
+        om.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);  // 忽略空bean转json错误
+        om.enable(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS);  // Map转换按key排序
+
         // 值为null字段不转换
-        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-        // 统一日期格式
-        objectMapper.setDateFormat(new SimpleDateFormat(TIME_PATTERN));
-        // 关闭默认转换timestamps格式
-        objectMapper.configure(SerializationFeature.WRITE_DATE_KEYS_AS_TIMESTAMPS, false);
-        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-        // 忽略空bean转json错误
-        objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
-        // 忽略json存在但Java对象中不存在的字段
-        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        return objectMapper;
+        om.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        // 统一日期格式：java.util.Date
+        om.setDateFormat(new SimpleDateFormat(TIME_PATTERN));
+        om.setTimeZone(TimeZone.getTimeZone(TIME_ZONE));
+        // Java8时间支持：LocalDate、LocalDateTime、LocalTime
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern(TIME_PATTERN);
+        javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(dtf));
+        javaTimeModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(dtf));
+        om.registerModule(javaTimeModule);
+
+        // 字节码增强模块
+        om.registerModule(new AfterburnerModule());
+        return om;
     }
 
     /**
@@ -51,15 +69,31 @@ public class Jackson {
      * @return json
      */
     public static <T> String toJson(T obj) {
-        if (obj == null) {
-            return null;
-        }
-        try {
-            return obj instanceof String ? obj.toString() : OBJECT_MAPPER.writeValueAsString(obj);
-        } catch (JsonProcessingException e) {
-            log.error("Parse Object to String error : {}", e.getMessage(), e);
+        if (obj != null) {
+            if (obj instanceof String) {
+                return (String) obj;
+            }
+            try {
+                return MAPPER.writeValueAsString(obj);
+            } catch (JsonProcessingException e) {
+                throw new JsonException(e);
+            }
         }
         return null;
+    }
+
+
+    /**
+     * 对象转 Json 带格式的字符串
+     * @param obj 对象
+     * @return json
+     */
+    public static <T> String toPrettyJson(T obj) {
+        try {
+            return MAPPER.writerWithDefaultPrettyPrinter().writeValueAsString(obj);
+        } catch (JsonProcessingException e) {
+            throw new JsonException(e);
+        }
     }
 
     /**
@@ -70,9 +104,9 @@ public class Jackson {
     public static <T> byte[] toBytes(T obj) {
         if (obj != null) {
             try {
-                return OBJECT_MAPPER.writeValueAsBytes(obj);
+                return MAPPER.writeValueAsBytes(obj);
             } catch (JsonProcessingException e) {
-                log.error("Parse Object to byte[] error : {}", e.getMessage(), e);
+                throw new JsonException(e);
             }
         }
         return null;
@@ -88,9 +122,9 @@ public class Jackson {
     public static <T> T toObject(String json, Class<T> clazz) {
         if (json != null && !json.isBlank() && clazz != null) {
             try {
-                return clazz.equals(String.class) ? (T) json : OBJECT_MAPPER.readValue(json, clazz);
+                return clazz.equals(String.class) ? (T) json : MAPPER.readValue(json, clazz);
             } catch (Exception e) {
-                log.error("Parse String to Object error : {}", e.getMessage(), e);
+                throw new JsonException(e);
             }
         }
         return null;
@@ -105,9 +139,9 @@ public class Jackson {
     public static <T> T toObject(byte[] bytes, Class<T> clazz) {
         if (bytes != null && bytes.length > 0 && clazz != null) {
             try {
-                return OBJECT_MAPPER.readValue(bytes, clazz);
+                return MAPPER.readValue(bytes, clazz);
             } catch (Exception e) {
-                log.error("Parse byte[] to Object error : {}", e.getMessage(), e);
+                throw new JsonException(e);
             }
         }
         return null;
@@ -117,40 +151,63 @@ public class Jackson {
      * Json字符串转 List 集合
      * @param json         json 字符串
      * @param elementClazz 集合元素类型
-     * @return 集合
+     * @return List
      */
-    @SuppressWarnings("unchecked")
     public static <T> List<T> toList(String json, Class<T> elementClazz) {
-        return toCollection(json, List.class, elementClazz);
+        if (json != null && !json.isBlank() && elementClazz != null) {
+            try {
+                return MAPPER.readValue(json, listOf(elementClazz));
+            } catch (JsonProcessingException e) {
+                throw new JsonException(e);
+            }
+        }
+        return null;
     }
 
     /**
      * Json字符串转 Set 集合
      * @param json         json 字符串
      * @param elementClazz 集合元素类型
-     * @return 集合
+     * @return Set
      */
-    @SuppressWarnings("unchecked")
     public static <T> Set<T> toSet(String json, Class<T> elementClazz) {
-        return toCollection(json, Set.class, elementClazz);
-    }
-
-    /**
-     * Json字符串转集合
-     * @param json            json 字符串
-     * @param collectionClazz 集合类型
-     * @param elementClazz    集合元素类型
-     * @return 集合
-     */
-    private static <T extends Collection<?>, E> T toCollection(String json, Class<T> collectionClazz, Class<E> elementClazz) {
-        if (json != null && collectionClazz != null && elementClazz != null) {
-            JavaType javaType = OBJECT_MAPPER.getTypeFactory().constructParametricType(collectionClazz, elementClazz);
+        if (json != null && !json.isBlank() && elementClazz != null) {
             try {
-                return OBJECT_MAPPER.readValue(json, javaType);
-            } catch (IOException e) {
-                log.error("Parse String to Object error : {}", e.getMessage(), e);
+                return MAPPER.readValue(json, setOf(elementClazz));
+            } catch (JsonProcessingException e) {
+                throw new JsonException(e);
             }
         }
         return null;
+    }
+
+    /**
+     * Json字符串转 Map 集合
+     * @param json       json 字符串
+     * @param keyClazz   集合键类型
+     * @param valueClazz 集合值类型
+     * @return Map
+     */
+    public static <K, V> Map<K, V> toMap(String json, Class<K> keyClazz, Class<V> valueClazz) {
+        if (json != null && !json.isBlank() && keyClazz != null && valueClazz != null) {
+            try {
+                return MAPPER.readValue(json, mapOf(keyClazz, valueClazz));
+            } catch (JsonProcessingException e) {
+                throw new JsonException(e);
+            }
+        }
+        return null;
+    }
+
+    private static <T> JavaType listOf(Class<T> elementClazz) {
+        return MAPPER.getTypeFactory().constructCollectionType(List.class, elementClazz);
+    }
+
+    private static <T> JavaType setOf(Class<T> elementClazz) {
+        return MAPPER.getTypeFactory().constructCollectionType(Set.class, elementClazz);
+    }
+
+    private static <K, V> JavaType mapOf(Class<K> keyClazz, Class<V> valueClazz) {
+        return MAPPER.getTypeFactory().constructMapType(Map.class, keyClazz, valueClazz);
     }
 }
