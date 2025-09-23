648fccdbb0f481e9c289005393578c9aab97477b
heycm <heycm@qq.com>
2025-09-23 18:58:04 +0800

    :beers: 租户上下文&MDC包装器

--- cloudx-core/src/main/java/com/github/heycm/cloudx/core/utils/wrapper/MdcWrapper.java
+++ cloudx-core/src/main/java/com/github/heycm/cloudx/core/utils/wrapper/MdcWrapper.java
@@ -1,70 +1,72 @@
 package com.github.heycm.cloudx.core.utils.wrapper;
 
-import com.github.heycm.cloudx.core.context.TenantContextHolder;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 import java.util.function.Supplier;
+import org.slf4j.MDC;
 
 /**
- * 租户上下文包装器，在线程中传递租户上下文
+ * MDC包装器，线程上下文传递
  * @author heycm
  * @version 1.0
- * @since 2025/9/23 18:49
+ * @since 2025/9/23 18:52
  */
-public class TenantContextWrapper {
+public class MdcWrapper {
 
-    private TenantContextWrapper() {
+    private MdcWrapper() {
         throw new UnsupportedOperationException("Utility class");
     }
 
+
     /**
-     * 包装执行单元以传递租户上下文
+     * 包装执行单元以传递MDC上下文
      * @param runnable 执行单元
      * @return 包装后的执行单元
      */
     public static Runnable wrap(Runnable runnable) {
-        String tenantId = TenantContextHolder.getTenantId();
+        Map<String, String> ctx = MDC.getCopyOfContextMap();
         long mainId = Thread.currentThread().threadId();
         return () -> {
             boolean isSwitch = mainId != Thread.currentThread().threadId();
             if (isSwitch) {
-                TenantContextHolder.setTenantId(tenantId);
+                MDC.setContextMap(ctx);
             }
             try {
                 runnable.run();
             } finally {
                 if (isSwitch) {
-                    TenantContextHolder.clear();
+                    MDC.clear();
                 }
             }
         };
     }
 
     /**
-     * 包装执行单元以传递租户上下文
+     * 包装执行单元以传递MDC上下文
      * @param supplier 执行单元
      * @return 包装后的执行单元
      */
     public static <U> Supplier<U> wrap(Supplier<U> supplier) {
-        String tenantId = TenantContextHolder.getTenantId();
+        Map<String, String> ctx = MDC.getCopyOfContextMap();
         long mainId = Thread.currentThread().threadId();
         return () -> {
             boolean isSwitch = mainId != Thread.currentThread().threadId();
             if (isSwitch) {
-                TenantContextHolder.setTenantId(tenantId);
+                MDC.setContextMap(ctx);
             }
             try {
                 return supplier.get();
             } finally {
                 if (isSwitch) {
-                    TenantContextHolder.clear();
+                    MDC.clear();
                 }
             }
         };
     }
 
     /**
-     * 创建一个异步任务，并传递租户上下文
+     * 创建一个异步任务，并传递 MDC
      * @param runnable 执行单元
      * @param executor 执行器
      * @return CompletableFuture
@@ -74,7 +76,7 @@ public class TenantContextWrapper {
     }
 
     /**
-     * 创建一个异步任务，并传递租户上下文
+     * 创建一个异步任务，并传递 MDC
      * @param supplier 执行单元
      * @param executor 执行器
      * @return CompletableFuture
@@ -82,5 +84,4 @@ public class TenantContextWrapper {
     public static <U> CompletableFuture<U> supplyAsync(Supplier<U> supplier, Executor executor) {
         return CompletableFuture.supplyAsync(wrap(supplier), executor);
     }
-
 }
